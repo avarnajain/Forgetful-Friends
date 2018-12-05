648708470a4e632b27e3ab17da0089e6ce707825
cynthia <cd13@gmail.com>
2018-12-04 22:47:18 -0600

    Changes between Avarana and Cynthia merged

--- app/src/main/java/com/example/cynthia/kasa/MainActivity.java
+++ app/src/main/java/com/example/cynthia/kasa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cynthia.kasa;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -12,6 +13,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.google.android.gms.common.api.Status;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private double longitude;
     private double latitude;
     private LatLng currentLocation;
+    private Button button;
 
 
 
@@ -58,6 +62,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
 
 
 
@@ -133,4 +144,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
 }
