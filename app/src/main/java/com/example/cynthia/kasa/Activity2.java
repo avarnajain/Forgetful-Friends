fd01b2e45a1f25b65ca3d24773837e00d1c7a85c
avarnajain <avarnaj@illinois.edu>
2018-12-11 12:16:33 -0600

    moved sms code to activity 2 - not tested yet

--- app/src/main/java/com/example/cynthia/kasa/Activity2.java
+++ app/src/main/java/com/example/cynthia/kasa/Activity2.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -78,7 +79,9 @@ public class Activity2 extends AppCompatActivity {
                 System.out.println("current location array: " + currentLocationArray[0] + " " + currentLocationArray[1]);
                 System.out.println("home: " + MainActivity.getHome2()[0] + " " + MainActivity.getHome2()[1]);
                 if (Arrays.equals(currentLocationArray, MainActivity.getHome2())) {
-                    System.out.println("It works!!");
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("smsto:1" + MainActivity.getNumber(), null, "Your friend has reached their destination!" , null, null);
+
                 }
             }
             @Override
